687d25a1a80c46734813b30b59f808a7c60edaa2
CPPServer <126841556+cppservergit@users.noreply.github.com>
2023-08-12 01:34:44 -0400

    Add files via upload

--- src/sql.cpp
+++ src/sql.cpp
@@ -2,86 +2,212 @@
 
 namespace 
 {
-	const std::string LOGGER_SRC {"sql"};
-	constexpr int max_retries{3};
-	constexpr int PG_DATE = 1082;
-	constexpr int PG_TIMESTAMP = 1114;
-	constexpr int PG_VARCHAR = 1043;
-	constexpr int PG_TEXT = 25;	
+	constexpr int max_retries {3};
+	const std::string LOGGER_SRC {"sql-odbc"};
+	thread_local std::string m_sqlstate{""};
 
-	//get a clean error message suitable for JSON logs
-	inline std::string get_error(PGconn* conn)
+	using record    = std::unordered_map<std::string, std::string>;
+	using recordset = std::vector<record>;
+
+	std::string get_error_msg(SQLHENV henv, SQLHDBC hdbc, SQLHSTMT hstmt, const std::string& sql="") 
 	{
-		std::string msg {PQerrorMessage(conn)};
-		if (auto pos = msg.find("\n"); pos != std::string::npos)
-			msg.erase(pos);
-		if (auto pos = msg.find("ERROR:  "); pos != std::string::npos)
-			msg.erase(0, pos + 8);
-		for (char& c: msg) 
-			if (c == '"') c = '\'';
-		return msg;
+	    unsigned char szSQLSTATE[10];
+	    SDWORD nErr;
+	    unsigned char msg[SQL_MAX_MESSAGE_LENGTH + 1];
+	    SWORD cbmsg;
+	    std::stringstream errMsg;
+	    SQLError(henv, hdbc, hstmt, szSQLSTATE, &nErr, msg, sizeof(msg), &cbmsg);
+		const std::string sqlState( reinterpret_cast< char const* >(szSQLSTATE) );
+		const std::string sqlErrorMsg( reinterpret_cast< char const* >(msg) );
+		errMsg << sqlState << " " << sqlErrorMsg; 
+		if (!sql.empty()) 
+			errMsg << " SQL: " << sql;
+		m_sqlstate = sqlState;
+		return errMsg.str();
+	}
+
+	struct col_info {
+		
+		std::string colname;
+		SQLSMALLINT dataType{0};
+		SQLLEN dataBufferSize{0};
+		SQLLEN dataSize{0};
+		std::vector<SQLCHAR> data;
+
+		col_info(std::string _colname, SQLSMALLINT _dataType, SQLLEN _dataBufferSize ):
+			colname{ _colname },
+			dataType{ _dataType },
+			dataBufferSize{ _dataBufferSize }
+		{
+			data.resize(dataBufferSize);
+		};
+		
+	};
+
+	inline std::vector<col_info> bind_cols(SQLHSTMT hstmt, SQLSMALLINT& numCols) {
+		
+		std::vector<col_info> cols;
+		cols.reserve( numCols );
+
+		for (int i = 0; i < numCols; i++) {
+			SQLCHAR colname[50];
+			SQLSMALLINT NameLength, dataType, DecimalDigits, Nullable;
+			SQLULEN ColumnSize;
+			SQLLEN displaySize;
+			SQLDescribeCol(hstmt, i + 1, colname, sizeof(colname), &NameLength, &dataType, &ColumnSize, &DecimalDigits, &Nullable);
+			SQLColAttribute(hstmt, i + 1, SQL_DESC_DISPLAY_SIZE, NULL, 0, NULL, &displaySize);
+			displaySize++;
+			col_info& col = cols.emplace_back( reinterpret_cast<char*>(colname), dataType, displaySize );
+			SQLBindCol(hstmt, i + 1, SQL_C_CHAR, &col.data[0], col.dataBufferSize, &col.dataSize);
+		}
+		
+		return cols;
 	}
 
 	struct dbutil 
 	{
 		
-		std::string m_dbconnstr{""};
-		PGconn* conn{nullptr};
+		std::string m_dbconnstr;
+		SQLHENV henv = SQL_NULL_HENV;
+		SQLHDBC hdbc = SQL_NULL_HDBC;
+		SQLHSTMT hstmt = SQL_NULL_HSTMT;
 				
 		dbutil() 
 		{
+			logger::log(LOGGER_SRC, "debug", "empty constructor invoked", true);
 		}
 		
 		dbutil(const std::string& conn_info): m_dbconnstr{conn_info}
 		{
-			conn = PQconnectdb(m_dbconnstr.c_str());
-			if (PQstatus(conn) != CONNECTION_OK)
-				logger::log(LOGGER_SRC, "error", std::string(__FUNCTION__) + ": " + get_error(conn), true);
+			RETCODE rc {SQL_SUCCESS};
+			rc = SQLAllocHandle ( SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv );
+			if ( rc != SQL_SUCCESS ) {
+				logger::log(LOGGER_SRC, "error", std::string(__FUNCTION__) + " SQLAllocHandle failed", true);
+			}
+
+			rc = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3 , 0 );
+			if ( rc != SQL_SUCCESS ) {
+				logger::log(LOGGER_SRC, "error", std::string(__FUNCTION__) + " SQLSetEnvAttr failed to set ODBC version", true);
+			}
+
+			SQLCHAR* dsn = (SQLCHAR*)m_dbconnstr.c_str();
+			SQLSMALLINT bufflen;
+			rc = SQLAllocHandle (SQL_HANDLE_DBC, henv, &hdbc);
+		
+			rc = SQLDriverConnect(hdbc, NULL, dsn, SQL_NTS, NULL, 0, &bufflen, SQL_DRIVER_NOPROMPT);
+			if (rc!=SQL_SUCCESS && rc!=SQL_SUCCESS_WITH_INFO) {
+				logger::log(LOGGER_SRC, "error", std::string(__FUNCTION__) + " SQLDriverConnect failed: " + get_error_msg(henv, hdbc, hstmt), true);
+			}
+
+			rc = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
 		}
 		
-		dbutil(dbutil &&source) : m_dbconnstr{source.m_dbconnstr}, conn{source.conn}
+		dbutil(dbutil &&source) : m_dbconnstr{source.m_dbconnstr}, henv{source.henv}, hdbc{source.hdbc}, hstmt{source.hstmt}
 		{
-			source.conn = nullptr;
+			source.henv = 0;
+			source.hdbc = 0;
+			source.hstmt = 0;
 		}
 
-		dbutil(const dbutil &source) : m_dbconnstr{source.m_dbconnstr}, conn{source.conn}
+		dbutil(const dbutil &source) : m_dbconnstr{source.m_dbconnstr}, henv{source.henv}, hdbc{source.hdbc}, hstmt{source.hstmt}
 		{
 		}
 		
 		~dbutil() {
-			if (conn) 
-				PQfinish(conn);
-		}
-		
-		inline void reset_connection() noexcept
-		{
-			if ( PQstatus(conn) == CONNECTION_BAD ) {
-				logger::log(LOGGER_SRC, "warn", std::string(__FUNCTION__) + ": connection to database " + std::string(PQdb(conn)) + " no longer valid, reconnecting... ", true);
-				PQfinish(conn);
-				conn = PQconnectdb(m_dbconnstr.c_str());
-				if (PQstatus(conn) != CONNECTION_OK)
-					logger::log(LOGGER_SRC, "error", std::string(__FUNCTION__) + ": error reconnecting to database " + std::string(PQdb(conn)) + " - " + get_error(conn), true);
-				else
-					logger::log(LOGGER_SRC, "info", std::string(__FUNCTION__) + ": connection to database " +  std::string(PQdb(conn)) + " restored", true);
+			if (henv) {
+				logger::log(LOGGER_SRC, "debug", "releasing odbc resources", true);
+				SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+				SQLDisconnect(hdbc);
+				SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+				SQLFreeHandle( SQL_HANDLE_ENV, henv );
 			}
+		}
+	
+		void reset_connection()
+		{
+			logger::log(LOGGER_SRC, "warn", "resetting odbc connection", true);
+			SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+			SQLDisconnect(hdbc);
+			SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+			SQLFreeHandle( SQL_HANDLE_ENV, henv );
+			RETCODE rc {SQL_SUCCESS};
+			rc = SQLAllocHandle ( SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv );
+			if ( rc != SQL_SUCCESS ) {
+				logger::log(LOGGER_SRC, "error", std::string(__FUNCTION__) + " SQLAllocHandle failed", true);
+			}
+
+			rc = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3 , 0 );
+			if ( rc != SQL_SUCCESS ) {
+				logger::log(LOGGER_SRC, "error", std::string(__FUNCTION__) + " SQLSetEnvAttr failed to set ODBC version", true);
+			}
+
+			SQLCHAR* dsn = (SQLCHAR*)m_dbconnstr.c_str();
+			SQLSMALLINT bufflen;
+			rc = SQLAllocHandle (SQL_HANDLE_DBC, henv, &hdbc);
+		
+			rc = SQLDriverConnect(hdbc, NULL, dsn, SQL_NTS, NULL, 0, &bufflen, SQL_DRIVER_NOPROMPT);
+			if (rc!=SQL_SUCCESS && rc!=SQL_SUCCESS_WITH_INFO) {
+				logger::log(LOGGER_SRC, "error", std::string(__FUNCTION__) + " SQLDriverConnect failed: " + get_error_msg(henv, hdbc, hstmt), true);
+			}
+
+			rc = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);				
 		}
 	
 	};
 
 	thread_local  std::unordered_map<std::string, dbutil> dbconns;
 
-	PGconn* getdb(const std::string& dbname)
+	recordset get_recordset(SQLHSTMT hstmt) 
 	{
-		if (!dbconns.contains(dbname)) {
-			std::string error{std::string(__FUNCTION__) + ": invalid dbname: " + dbname};
-			throw std::runtime_error(error.c_str());
+		recordset rs;
+		SQLSMALLINT numCols ;
+		SQLNumResultCols( hstmt, &numCols );
+		if (numCols>0) {
+			auto cols = bind_cols( hstmt, numCols );
+			while ( SQLFetch( hstmt )!=SQL_NO_DATA ) {
+				record rec;
+				rec.reserve( numCols );
+				for ( auto& col: cols ) {
+					if (col.dataSize > 0) {
+						rec[col.colname] = reinterpret_cast<char*>(&col.data[0]);
+					} else {
+						rec[col.colname] = "";
+					}
+				}
+				rs.push_back(rec);
+			}
 		}
-		return dbconns[dbname].conn;
-	}
+		return rs;
+	}	
 
-	void reset(const std::string& dbname) noexcept
-	{
-		dbconns[dbname].reset_connection();
+	void get_json_array(SQLHSTMT hstmt, std::string &json) {
+		json.append("[");
+		SQLSMALLINT numCols{0};
+		SQLNumResultCols( hstmt, &numCols );
+		if (numCols > 0) {
+			auto cols = bind_cols( hstmt, numCols );
+			while (SQLFetch(hstmt)!=SQL_NO_DATA) {
+				json.append("{");
+				for (auto& col: cols) {
+					json.append("\"").append(col.colname).append("\":");
+					if ( col.dataSize > 0 ) {
+						if (col.dataType==SQL_TYPE_DATE || col.dataType==SQL_VARCHAR || col.dataType==SQL_WVARCHAR || col.dataType==SQL_CHAR) {
+							json.append("\"").append(  reinterpret_cast<char const*>(&col.data[0]) ).append("\"");
+						} else {
+							json.append( reinterpret_cast<char const*>(&col.data[0]) );
+						}
+					} else {
+						json.append("\"\"");
+					}
+					json.append(",");
+				}
+				json.pop_back();
+				json.append("},");
+			}
+			if (json.back() == ',')
+				json.pop_back();
+		}
+	    json.append("]");
 	}
 
 }
@@ -92,151 +218,181 @@ namespace sql
 	{
 		if (!dbconns.contains(dbname)) { 
 			dbconns.insert({dbname, dbutil(conn_info)});
-		}
-		else {
-			std::string error{std::string(__FUNCTION__) + ": duplicated dbname: " + dbname};
+		} else {
+			std::string error{std::string(__FUNCTION__) + " duplicated dbname: " + dbname};
 			throw std::runtime_error(error.c_str());
 		}
 	}
 
-	//executes a query that doesn't return rows (data modification query)
-	void exec_sql(const std::string& dbname, const std::string& sql)
+	dbutil& getdb(const std::string& dbname)
 	{
-		int retries {0};
-	retry:
-		PGconn *conn = getdb(dbname);
-		PGresult *res = PQexec(conn, sql.c_str());
-		if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-			PQclear(res);
-			if ( PQstatus(conn) == CONNECTION_BAD ) {
-				if (retries == max_retries) {
-					std::string error_message{"cannot connect to database: " + dbname};
-					throw std::runtime_error(error_message);
-				} else {
-					retries++;
-					reset(dbname);
-					goto retry;
-				}
-			} else {
-				std::string error {get_error(conn) + " sql: " + sql};
-				throw std::runtime_error(error);
-			}
+		if (!dbconns.contains(dbname)) {
+			std::string error{std::string(__FUNCTION__) + " invalid dbname: " + dbname};
+			throw std::runtime_error(error.c_str());
 		}
-		PQclear(res);
-	}
-	
-	//returns true if the query retuned 1+ row
-	bool has_rows(const std::string& dbname, const std::string &sql)
-	{
-		int retries {0};
-	
-	retry:
-		PGconn *conn = getdb(dbname);
-		PGresult *res = PQexec(conn, sql.c_str());
-		
-		if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-			PQclear(res);
-			if ( PQstatus(conn) == CONNECTION_BAD ) {
-				if (retries == max_retries) {
-					std::string error_message{"cannot connect to database: " + dbname};
-					throw std::runtime_error(error_message);
-				} else {
-					retries++;
-					reset(dbname);
-					goto retry;
-				}
-			} else {
-				std::string error {get_error(conn) + " sql: " + sql};
-				throw std::runtime_error(error);
-			}
-		}
-		
-		bool result {true};
-		if (PQntuples(res) == 0)
-			result = false;
-		PQclear(res);
-		return result;
+		return dbconns[dbname];
 	}	
 	
-	//returns only the first rows of a resultset, use of "limit 1" or "where col=pk" in the query is recommended
-	std::unordered_map<std::string, std::string> get_record(const std::string& dbname, const std::string& sql)
+	bool has_rows(const std::string& dbname, const std::string& sql)
 	{
+		recordset rs;
+		SQLCHAR* sqlcmd = (SQLCHAR*)sql.c_str();
+		dbutil& db{getdb(dbname)};
+		RETCODE rc {SQL_SUCCESS};
 		int retries {0};
-		std::unordered_map<std::string, std::string> rec;
-		rec.reserve(5);
+		m_sqlstate = "";
 		
-	retry:
-		PGconn *conn = getdb(dbname);	
-		PGresult *res = PQexec(conn, sql.c_str());
-		
-		if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-			PQclear(res);
-			if ( PQstatus(conn) == CONNECTION_BAD ) {
-				if (retries == max_retries) {
-					std::string error_message{"cannot connect to database: " + dbname};
-					throw std::runtime_error(error_message);
-				} else {
-					retries++;
-					reset(dbname);
-					goto retry;
-				}
-			} else {
-				std::string error {get_error(conn) + " sql: " + sql};
-				throw std::runtime_error(error);
-			}
+	retry:		
+		rc = SQLExecDirect(db.hstmt, sqlcmd, SQL_NTS);
+		if (rc != SQL_SUCCESS) {
+			std::string error{std::string(__FUNCTION__) + + " " + get_error_msg(db.henv, db.hdbc, db.hstmt, sql)};
+			if (retries < max_retries && (m_sqlstate == "01000" || m_sqlstate == "08S01" || rc == SQL_INVALID_HANDLE)) {
+				++retries;
+				db.reset_connection();
+				goto retry;
+			}			
+			throw std::runtime_error(error.c_str());				
+		} else {
+			rs = get_recordset(db.hstmt);
 		}
-		
-		int rows {PQntuples(res)};
-		int cols {PQnfields(res)};
-		if (rows) {
-			for(int j=0; j<cols; j++) {
-				rec.emplace(PQfname(res, j), PQgetvalue(res, 0, j));
-			}
-		}
-		PQclear(res);
-		return rec;
+
+		SQLFreeStmt(db.hstmt, SQL_CLOSE);
+		SQLFreeStmt(db.hstmt, SQL_UNBIND);
+		return (rs.size() > 0);
 	}
 
-	//executes SQL that returns a single row with a single column containing a JSON response when data is available
-	//throws exception in case of database error
-	//returns JSON response with status OK or EMPTY (if query finds no rows)
-	std::string get_json_response(const std::string& dbname, const std::string &sql)
+	std::unordered_map<std::string, std::string> get_record(const std::string& dbname, const std::string& sql)
 	{
+		recordset rs;
+		record rec;
+		SQLCHAR* sqlcmd = (SQLCHAR*)sql.c_str();
+		dbutil& db{getdb(dbname)};
+		RETCODE rc {SQL_SUCCESS};
 		int retries {0};
+		m_sqlstate = "";
 		
-	retry:
-		PGconn *conn = getdb(dbname);	
-		PGresult *res = PQexec(conn, sql.c_str());
-		
-		if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-			PQclear(res);
-			if ( PQstatus(conn) == CONNECTION_BAD ) {
-				if (retries == max_retries) {
-					std::string error_message{"cannot connect to database: " + dbname};
-					throw std::runtime_error(error_message);
-				} else {
-					retries++;
-					reset(dbname);
-					goto retry;
-				}
-			} else {
-				std::string error {get_error(conn) + " sql: " + sql};
-				throw std::runtime_error(error);
-			}
+	retry:		
+		rc = SQLExecDirect(db.hstmt, sqlcmd, SQL_NTS);
+
+		if (rc != SQL_SUCCESS) {
+			std::string error{std::string(__FUNCTION__) + + " " + get_error_msg(db.henv, db.hdbc, db.hstmt, sql)};
+			if (retries < max_retries && (m_sqlstate == "01000" || m_sqlstate == "08S01" || rc == SQL_INVALID_HANDLE)) {
+				++retries;
+				db.reset_connection();
+				goto retry;
+			}			
+			throw std::runtime_error(error.c_str());
+		} else {
+			rs = get_recordset(db.hstmt);
+			if (rs.size())
+				rec = rs[0];
 		}
+
+		SQLFreeStmt(db.hstmt, SQL_CLOSE);
+		SQLFreeStmt(db.hstmt, SQL_UNBIND);
+		return rec;
+	}
 	
-		std::string json;
-		int rows {PQntuples(res)};
-		bool is_null {0};
-		if (rows) {
-			is_null = PQgetisnull(res, 0, 0);
-			if (!is_null) 
-				json.append("{\"status\":\"OK\", \"data\":").append(PQgetvalue(res, 0, 0)).append("}");
+	std::string get_json_response(const std::string& dbname, const std::string &sql, bool useDataPrefix, const std::string &prefixName)
+	{
+		std::string json; json.reserve(16383);
+		dbutil& db{getdb(dbname)};
+		RETCODE rc{SQL_SUCCESS};		
+		SQLCHAR* sqlcmd = (SQLCHAR*)sql.c_str();
+		int retries {0};
+		m_sqlstate = "";
+		
+	retry:	
+		rc = SQLExecDirect(db.hstmt, sqlcmd, SQL_NTS);
+		if (rc != SQL_SUCCESS) {
+			std::string error{std::string(__FUNCTION__) + + " " + get_error_msg(db.henv, db.hdbc, db.hstmt, sql)};
+			if (retries < max_retries && (m_sqlstate == "01000" || m_sqlstate == "08S01" || rc == SQL_INVALID_HANDLE)) {
+				++retries;
+				db.reset_connection();
+				goto retry;
+			}
+			throw std::runtime_error(error.c_str());
+		} else {
+			if (useDataPrefix) {
+				json.append( R"({"status":"OK",)" );
+				json.append("\"");
+				json.append(prefixName);
+				json.append("\":");
+			}
+			get_json_array(db.hstmt, json);
+			if (useDataPrefix)
+				json.append("}");
 		}
-		if (!rows || is_null)
-			json.append("{\"status\":\"EMPTY\"}");
-		PQclear(res);
+
+		SQLFreeStmt(db.hstmt, SQL_CLOSE);
+		SQLFreeStmt(db.hstmt, SQL_UNBIND);
+		return json;
+	}
+
+	std::string get_json_response(const std::string& dbname, const std::string &sql, const std::vector<std::string> &varNames, const std::string &prefixName) 
+	{
+		std::string json; json.reserve(16383);
+		dbutil& db{getdb(dbname)};
+		RETCODE rc{SQL_SUCCESS};		
+		SQLCHAR* sqlcmd = (SQLCHAR*)sql.c_str();
+		int retries {0};
+		m_sqlstate = "";
+		
+	retry:		
+		rc = SQLExecDirect(db.hstmt, sqlcmd, SQL_NTS);
+		if (rc != SQL_SUCCESS) {
+			std::string error{std::string(__FUNCTION__) + + " " + get_error_msg(db.henv, db.hdbc, db.hstmt, sql)};
+			if (retries < max_retries && (m_sqlstate == "01000" || m_sqlstate == "08S01" || rc == SQL_INVALID_HANDLE)) {
+				++retries;
+				db.reset_connection();
+				goto retry;
+			}
+			throw std::runtime_error(error.c_str());
+		} else {
+			int rowsetCounter{0};
+			json.append(R"({"status":"OK",)");
+			json.append("\"");
+			json.append(prefixName);
+			json.append("\":{");
+			do {
+				json.append( "\"");
+				json.append(varNames[rowsetCounter]);
+				json.append("\":");
+				get_json_array(db.hstmt, json);
+				json.append(",");
+				++rowsetCounter;
+			} while (SQLMoreResults(db.hstmt) == SQL_SUCCESS);
+			json.pop_back(); //remove last coma ","
+			json.append("}}");
+		}
+
+		SQLFreeStmt(db.hstmt, SQL_CLOSE);
+		SQLFreeStmt(db.hstmt, SQL_UNBIND);
 		return json;
 	}
 	
+	void exec_sql(const std::string& dbname, const std::string& sql)
+	{
+		dbutil& db{getdb(dbname)};
+		RETCODE rc{SQL_SUCCESS};		
+		SQLCHAR* sqlcmd = (SQLCHAR*)sql.c_str();
+		int retries {0};
+		m_sqlstate = "";
+		
+	retry:	
+		rc = SQLExecDirect(db.hstmt, sqlcmd, SQL_NTS);
+		if (rc != SQL_SUCCESS && rc!=SQL_NO_DATA) {
+			std::string error{std::string(__FUNCTION__) + + " " + get_error_msg(db.henv, db.hdbc, db.hstmt, sql)};
+			if (retries < max_retries && (m_sqlstate == "01000" || m_sqlstate == "08S01" || rc == SQL_INVALID_HANDLE)) {
+				++retries;
+				db.reset_connection();
+				goto retry;
+			}
+			throw std::runtime_error(error.c_str());
+		}
+
+		SQLFreeStmt(db.hstmt, SQL_CLOSE);
+	}
 }
+
+
--- src/sql.h
+++ src/sql.h
@@ -1,29 +1,38 @@
 /*
- * sql - json microservices PGSQL utility API - depends on libpq (pgsql native client API)
+ * sql - json microservices ODBC utility API - depends on  unixodbc-dev
  *
- *  Created on: Feb 23, 2023
- *      Author: Martin Cordova cppserver@martincordova.com - https://cppserver.com
+ *  Created on: March 23, 2023
+ *      Author: Martín Córdova cppserver@martincordova.com - https://cppserver.com
  *      Disclaimer: some parts of this library may have been taken from sample code publicly available
  *		and written by third parties. Free to use in commercial projects, no warranties and no responsabilities assumed 
  *		by the author, use at your own risk. By using this code you accept the forementioned conditions.
  */
-#ifndef SQL_H_
-#define SQL_H_
+#ifndef SQLODBC_H_
+#define SQLODBC_H_
 
-#include <string>
+#include <sql.h>
+#include <sqlext.h>
 #include <iostream>
-#include <unordered_map>
+#include <string>
+#include <cstdlib>
+#include <sstream>
 #include <vector>
-#include <libpq-fe.h>
+#include <chrono>
+#include <thread>
+#include <sstream>
+#include <iomanip>
+#include <unordered_map>
+#include <string.h>
 #include "logger.h"
 
 namespace sql
 {
 	void connect(const std::string& dbname, const std::string& conn_info);
+	std::string get_json_response(const std::string& dbname, const std::string &sql, bool useDataPrefix=true, const std::string &prefixName="data");
+	std::string get_json_response(const std::string& dbname, const std::string &sql, const std::vector<std::string> &varNames, const std::string &prefixName="data");
 	void exec_sql(const std::string& dbname, const std::string& sql);
 	bool has_rows(const std::string& dbname, const std::string &sql);
 	std::unordered_map<std::string, std::string> get_record(const std::string& dbname, const std::string& sql);
-	std::string get_json_response(const std::string& dbname, const std::string &sql);
 }
 
-#endif /* SQL_H_ */
+#endif /* SQLODBC_H_ */
