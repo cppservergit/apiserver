7a8eae0dfa0b665ca0b0c3ed322dfd09a6a904ba
CPPServer <126841556+cppservergit@users.noreply.github.com>
2023-10-24 12:01:53 -0400

    Add files via upload
    The library was modified to use Ubuntu default `libjson-c` JSON parser library for C instead of a built-in parser, the code was reduced to a minimum

--- src/json.h
+++ src/json.h
@@ -14,6 +14,7 @@
 #include <string_view>
 #include <ranges>
 #include <unordered_map>
+#include <json-c/json.h>
 #include "util.h"
 
 namespace json
@@ -23,58 +24,28 @@ namespace json
 		public:
 			explicit invalid_json_exception(const std::string& _msg): m_msg {_msg} {}
 			std::string what() const noexcept {
-				std::string error_msg{m_msg};
-				return error_msg;
+				return m_msg;
 			}
 		private:
             std::string m_msg;
 	};		
 
-    constexpr void extract_value(std::string& s) noexcept
-    {
-        if (auto pos1 = s.find("\""); pos1 != std::string::npos) {
-            pos1 += 1;
-			if (auto pos2 = s.find("\"", pos1); pos2 != std::string::npos)
-                s = s.substr(pos1, pos2 - pos1); 
-            return;
-        }
-        if (s.empty()) return;
-        s.erase(0, s.find_first_not_of(" "));
-        s.erase(s.find_last_not_of(" ") + 1);
-    }
-
-	constexpr std::pair<std::string, std::string> split_value(std::string_view line)
-	{
-		if (const auto newpos = line.find(":", 0); newpos != std::string_view::npos) {
-			std::string header_name {line.substr(0,  newpos)};
-			std::string header_value {line.substr(newpos + 1,  line.size() - newpos)};
-			extract_value(header_name);
-			extract_value(header_value);
-			return std::make_pair(header_name, header_value);
-		} 
-		return std::make_pair("", "");
-	}
-
 	inline auto parse(std::string_view json)
 	{
 		std::unordered_map<std::string, std::string, util::string_hash, std::equal_to<>> fields;
-        if (auto pos1 = json.find("{"); pos1 != std::string::npos) {
-            pos1 += 1;
-			if (auto pos2 = json.find("}", pos1); pos2 != std::string::npos) {
-                const std::string_view body {json.substr(pos1, pos2 - pos1)}; 
-                const std::string delim{","};
-                for (const auto& word : std::views::split(body, delim)) {
-                    const auto& [name, value] {split_value(std::string_view{word})};
-                    fields.try_emplace(name, value);
-		        }   
-			} else
-				throw invalid_json_exception("invalid JSON format - lacks closing brace");
-		} else
-			throw invalid_json_exception("invalid JSON format - lacks opening brace");
-		if (fields.empty())
-			throw invalid_json_exception("invalid JSON format - no attributes available inside the braces");
+		json_object * jobj = json_tokener_parse(json.data());
+		if (jobj == nullptr) {
+				std::clog << std::format("[DEBUG][JSON] invalid JSON format: {}{}", json, "\n");
+				throw invalid_json_exception("invalid JSON format, check stderr log for details");
+		}
+		json_object_object_foreach(jobj, key, val) {
+				if (const char* val_ptr {json_object_get_string(val)})
+						fields.try_emplace(key, val_ptr);
+				else
+						fields.try_emplace(key, "");
+		}
 		return fields;
-	}	
+	}
 }
 
 #endif /* JSON_H_ */
